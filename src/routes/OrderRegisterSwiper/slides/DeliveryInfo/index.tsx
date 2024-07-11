2fe64c1dd7129b0103f28315efb1055e213de040
marcelocab200 <marcelocab200@gmail.com>
2024-07-11 02:59:20 -0300

    UI/UX adjustments

--- src/routes/OrderRegisterSwiper/slides/DeliveryInfo/index.tsx
+++ src/routes/OrderRegisterSwiper/slides/DeliveryInfo/index.tsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 
 import TextInput from "../../../../components/UI/Input/TextInput";
 import { useOrderContext } from "../../../../context/OrderContext";
 import { getTravelTimeORS } from "../../../../services/api";
 
+import DeliveryImg from "../../../../assets/Delivery.png";
+import { CircularProgress } from "@mui/material";
+
 export default function DeliveryInfo() {
   const { orderAddress, setOrderAddress, orderTravelTime, setOrderTravelTime } =
     useOrderContext();
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleCalcDeliveryTime() {
+    setIsLoading(true);
     try {
       const response = await getTravelTimeORS(orderAddress);
       setOrderTravelTime(response); // Tempo de viagem em segundos
     } catch (e: any) {
       console.log(e);
     }
+    setIsLoading(false);
   }
 
   return (
     <div className="Delivery-info-container">
       <div className="Delivery-info">
         <h1>Só falta levar o pedido até você!</h1>
-        <p></p>
+        <img src={DeliveryImg} alt="Delivery Icon" />
         <div className="search-input-container">
           <TextInput
             label="Qual seu endereço?"
@@ -32,20 +38,33 @@ export default function DeliveryInfo() {
               setOrderAddress(e.target.value)
             }
           />
-          <button className="yellow-button" onClick={handleCalcDeliveryTime}>
-            Calcular
+          <button
+            className="yellow-button"
+            onClick={handleCalcDeliveryTime}
+            disabled={!orderAddress}
+          >
+            {isLoading ? (
+              <CircularProgress size={20} color="inherit" />
+            ) : (
+              "Calcular entrega"
+            )}
           </button>
         </div>
         {orderTravelTime > 0 && (
-          <>
-            <p>{`Tempo de viagem: ${Math.round(
-              orderTravelTime / 60
-            )} minutos.`}</p>
-            <p>{`Tempo de preparo: 30 minutos.`}</p>
-            <p>{`Tempo de entrega: ${
-              Math.round(orderTravelTime / 60) + 30
-            } minutos.`}</p>
-          </>
+          <div className={"Time-info"}>
+            <div className={"Lower-text"}>
+              <p>Tempo de viagem: </p>
+              <p>{`${Math.round(orderTravelTime / 60)} minutos`}</p>
+            </div>
+            <div className={"Lower-text"}>
+              <p>Tempo de preparo: </p>
+              <p>{`30 minutos`}</p>
+            </div>
+            <div className={"Lower-text"}>
+              <p>Tempo de entrega: </p>
+              <p>{`${Math.round(orderTravelTime / 60) + 30} minutos`}</p>
+            </div>
+          </div>
         )}
       </div>
     </div>
